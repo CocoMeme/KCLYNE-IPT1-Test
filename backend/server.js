55f0ff0cf44851ea2cf0718cb21c0ddcd02601f8
CocoMeme <coandrei04@gmail.com>
2024-09-12 10:15:30 +0800

    First Commit

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+//const express = require('express');
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js"; 
+
+import productRoutes from "./routes/product.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use("/api/products", productRoutes);
+
+// console.log(process.env.MONGO_URI);
+app.listen(5000, () => {
+    connectDB();
+    console.log("KCLYNE Server is now starting at http://localhost:" + PORT)
+});
